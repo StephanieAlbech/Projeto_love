e62611f82faf34651b19891e37504cd9c19d3d6c
Alira666 <134435390+StephanieAlbech@users.noreply.github.com>
2023-06-03 22:54:09 -0300

    love

--- Curso JS/Aula 12/Ex011.js
+++
@@ -1,9 +0,0 @@
-var idade = 25
-console.log(`Você tem ${idade} Anos.`)
-if (idade < 16) {
-    console.log('Não vota')
-} else  if ( idade < 18 || idade >= 65) {
-    console.log('Voto Opcional')
-} else {
-    console.log('Voto Obrigatório')
-}
--- Curso JS/Aula17/Objeto01.js
+++
@@ -1,9 +0,0 @@
-var amigo = {nome:'Josse',
-sexo: 'masculino',
-peso: 84.5,
-engordar(p=0){
-    console.log('Engordou')
-    this.peso += p
-}}
-amigo.engordar(2)
-console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
